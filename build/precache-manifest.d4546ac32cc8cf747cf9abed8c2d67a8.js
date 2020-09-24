c0c3e01afd7e71e4b9cf5ce87da00b008dd6dee4
JakevK <jakevankeulen@gmail.com>
2020-09-24 13:01:53 +1200

    hhh

---
+++ build/precache-manifest.d4546ac32cc8cf747cf9abed8c2d67a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc573c08677e4af5e79887f2fdeab5df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48666e791e7fbca991e",
+    "url": "/static/css/main.e7f12c4f.chunk.css"
+  },
+  {
+    "revision": "caba7a7d0ae0fc0d59ea",
+    "url": "/static/js/2.78d8ec2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78d8ec2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48666e791e7fbca991e",
+    "url": "/static/js/main.83bc7c90.chunk.js"
+  },
+  {
+    "revision": "5cf3b2208cdd16e8bcc2",
+    "url": "/static/js/runtime-main.76363dd8.js"
+  },
+  {
+    "revision": "c7f0f255fbd269a1584f5c2b5d67d3a1",
+    "url": "/static/media/alphabet.c7f0f255.png"
+  },
+  {
+    "revision": "3ef9ed23b299ee0db756ce7ad446dac0",
+    "url": "/static/media/left_arrow.3ef9ed23.svg"
+  },
+  {
+    "revision": "9221b477666513bcd88cd257062a6eaf",
+    "url": "/static/media/telegraph.9221b477.gif"
+  }
+]);
